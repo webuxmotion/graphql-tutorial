6c6b6424c9a70473a21f69823de7da377081ce15
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-04-17 08:04:05 +0300

    refactor app

--- application/src/App.js
+++ application/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Tabs from './components/Tabs/Tabs';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import theme from './components/theme';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
