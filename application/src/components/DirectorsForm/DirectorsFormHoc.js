b7fa6f6d9dfa3a19c10c1d61d3f28bf31cc82026
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-04-17 17:31:54 +0300

    Adding Elements to DB

--- application/src/components/DirectorsForm/DirectorsFormHoc.js
+++ application/src/components/DirectorsForm/DirectorsFormHoc.js
@@ -1,6 +1,18 @@
 import { withStyles } from '@material-ui/core/styles';
 import { compose } from 'recompose';
-
+import { graphql } from 'react-apollo';
 import { styles } from './styles';
 
-export default compose(withStyles(styles));
+import { addDirectorMutation } from "./mutations";
+import { directorsQuery } from "../DirectorsTable/queries";
+
+const withGraphqlAdd = graphql(addDirectorMutation, {
+    props: ({ mutate }) => ({
+        addDirector: director => mutate({
+            variables: director,
+            refetchQueries: [{ query: directorsQuery }],
+        })
+    })
+})
+
+export default compose(withStyles(styles), withGraphqlAdd);
