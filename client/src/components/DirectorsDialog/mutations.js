c7cb5c1883bc836b8024fa6f98e1c8d6aad05570
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-04-20 18:32:50 +0300

    Add concurrently

--- client/src/components/DirectorsDialog/mutations.js
+++ client/src/components/DirectorsDialog/mutations.js
@@ -2,7 +2,7 @@ import { gql } from 'apollo-boost';
 
 export const deleteDirectorMutation = gql`
   mutation deleteDirector($id: ID) {
-    deleteMovie(id: $id) {
+    deleteDirector(id: $id) {
       id
     }
   }
